ebba711107c36c412d545cae3328fd29ef4375a3
miha11-hash <d_mihaela@msn.com>
2024-08-18 06:48:44 +0300

    Profile - protected route

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,10 +1,16 @@
 import { redirect } from "next/navigation";
 import Link from "next/link";
-
+import { getUserFromToken } from "../actions";
+import { cookies } from "next/headers";
 
 
 async function ProfilePage() {
-  
+  const cookieStore = cookies();
+  const authToken = cookieStore.get("auth_token")?.value;
+  if(!authToken) {
+    redirect("/");
+  }
+
   return (
     <div>
       <h1>Profile page</h1>
